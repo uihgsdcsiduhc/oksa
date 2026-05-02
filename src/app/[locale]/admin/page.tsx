31b93a68e758c7257c46c26d7175278867f696b1
uihgsdcsiduhc <sanael.tv.fr@gmail.com>
2026-05-02 15:33:29 +0200

    Delete src directory

--- src/app/[locale]/admin/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default async function AdminRootPage({ params }: { params: Promise<{ locale: string }> }) {
-  const { locale } = await params
-  redirect(`/${locale}/admin/login`)
-}
--- src/app/[locale]/tombola/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// Tombola page redirects to home (form is on homepage)
-export default async function TombolaPage({ params }: { params: Promise<{ locale: string }> }) {
-  const { locale } = await params
-  redirect(`/${locale}#inscription`)
-}
